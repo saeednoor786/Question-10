4a9261069ed09dab9607fe7d3f0e8af36f0f3805
Saeed Noor <163897168+saeednoor786@users.noreply.github.com>
2024-05-23 22:02:17 +0500

    ten assignment

---
+++ app.ts
@@ -0,0 +1,16 @@
+
+//Q:10 Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each.
+//If you don’t have anything specific to write because your programs are too simple at this point, just 
+// add your name and the current date at the top of each program file. 
+// Then write one sentence describing what the program does.
+
+// saeed_noor
+// friday/03/20
+
+// store my favorite number in a variable
+const favoriteNumber:number = 50;
+// creat a message with favorite number 
+const message:string = (`my favoriteNumber is , ${favoriteNumber}`);
+
+console.log(message);
+
